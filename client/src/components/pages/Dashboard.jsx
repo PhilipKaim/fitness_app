04d6a19a6c47dc4804d63e8908ec62b81922b952
Chillup <philipkaim@gmail.com>
2019-05-03 16:06:16 -0400

    moved fetching of all user data to the global scope

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -1,11 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import DatePicker from '../DatePicker'
 import NavBar from '../presentational/NavBar'
 
-import { getUser } from '../../actions/user'
-
-import queryString from 'query-string'
-import axios from 'axios'
 import { connect } from 'react-redux'
 import Aside from '../presentational/Aside';
 import PieChartUser from '../PieChartUser';
@@ -13,30 +9,7 @@ import PieChartLoseWeight from '../PieChartLoseWeight';
 import PieChartGainMuscle from '../PieChartGainMuscle';
 
 const Dashboard = (props) => {
-
-    useEffect(() => {
-        let query = queryString.parse(window.location.search)
-        
-        if (query.token) {
-            window.localStorage.setItem("jwt", query.token);
-        }
-        
-        if (window.localStorage.getItem('jwt') === null) {
-            window.location.href = '/home'
-        } else {
-            const token = window.localStorage.getItem('jwt');
-
-            async function fetchUser() {
-                let user = await axios.get(`/api/getUser/${token}`);
-
-                props.dispatch(getUser(user.data[0]))
-            }
-
-            fetchUser();
-        }
-    }, [])
     
-
     return (
         <div>
             <NavBar signout={ true } />
--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import queryString from 'query-string'
+import axios from 'axios'
+import { connect } from 'react-redux';
+
 import Login from '../components/pages/Login.jsx';
 import ManageFood from '../components/pages/ManageFood.jsx';
 import Error from '../components/pages/Error.jsx';
@@ -7,7 +11,32 @@ import Form from '../components/pages/Form.jsx';
 import Dashboard from '../components/pages/Dashboard.jsx';
 import Settings from '../components/pages/Settings';
 
+import { getUser } from '../actions/user'
+
+
 const AppRouter = (props) => {
+
+  useEffect(() => {
+      let query = queryString.parse(window.location.search)
+      
+      if (query.token) {
+          window.localStorage.setItem("jwt", query.token);
+      }
+      
+      if (window.localStorage.getItem('jwt') === null) {
+          window.location.href = '/home'
+      } else {
+          const token = window.localStorage.getItem('jwt');
+
+          async function fetchUser() {
+              let user = await axios.get(`/api/getUser/${token}`);
+
+              props.dispatch(getUser(user.data[0]))
+          }
+
+          fetchUser();
+      }
+  }, [])
 
     return (
       <div>
@@ -26,4 +55,10 @@ const AppRouter = (props) => {
 
 };
 
-export default AppRouter;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  };
+};
+
+export default connect(mapStateToProps)(AppRouter);
