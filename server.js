1bbfe186f2e700ee72b5e5051be0cdd325b6a74c
Chillup <philipkaim@gmail.com>
2019-05-07 23:00:58 -0400

    server script added to get client code

--- server.js
+++ server.js
@@ -31,6 +31,15 @@ app.use('/api', manageFoodRoutes)
 app.use('/api', dataRoutes)
 app.use('/api', formRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
