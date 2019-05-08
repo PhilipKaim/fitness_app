c8864abe6ec659a58656a3ac2e6467e5c7b08d01
Chillup <philipkaim@gmail.com>
2019-05-07 23:51:53 -0400

    allow config for production environment

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env === 'test') {
+if (env === 'development' || env === 'test' || env === 'production') {
   var config = require('./config.json');
   var envConfig = config[env];
   
